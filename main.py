b18e4f408d938654afa4c2bfe09d61301f406cb7
profmohamed <mohamedsalem.ca@gmail.com>
2023-02-21 17:48:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+# This is a sample Python script for coffe machine.
+# This script is written by mohamed salem mohamed-salem.com
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+is_on = True
+
+while is_on:
+    choise = input("What would you like? (espresso/latte/cappuccino): ")
+    if choise == "off":
+        is_on = False
+    elif choise == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choise]
+        if is_resource_sufficient(drink["ingredients"]):
+
